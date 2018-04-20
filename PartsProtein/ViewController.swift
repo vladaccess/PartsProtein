712f5f3d48a884b610400e75e52fa43450062d7c
Vlad Krasnokutskii <vladaccess@outlook.com>
2018-04-20 23:46:05 +0300

    Initial Commit

---
+++ PartsProtein/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PartsProtein
+//
+//  Created by attom on 20.04.2018.
+//  Copyright © 2018 attom. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
