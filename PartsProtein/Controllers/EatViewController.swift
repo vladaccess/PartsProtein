8eaf7a29a4a50e6ea4edac54f7e719ae648a836b
Vlad Krasnokutskii <vladaccess@outlook.com>
2018-04-25 23:35:31 +0300

    add UI

--- PartsProtein/Controllers/EatViewController.swift
+++ PartsProtein/Controllers/EatViewController.swift
@@ -7,24 +7,22 @@
 //
 
 import UIKit
+import CoreMotion
+
 
 class EatViewController: UIViewController {
+    
+    @IBOutlet weak var smallButton:UIButton!
+    @IBOutlet weak var largeButton:UIButton!
+    @IBOutlet weak var addButton:UIButton!
+    @IBOutlet weak var starButton:UIButton!
+    @IBOutlet weak var minusButton:UIButton!
+    @IBOutlet weak var containerView:UIView!
+    @IBOutlet weak var maskImage:UIImageView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "Eat!"
     }
-
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
