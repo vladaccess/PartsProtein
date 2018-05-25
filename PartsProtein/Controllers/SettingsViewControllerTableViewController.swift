f697c48b9717547928d324ef7155e4ab5143e8a5
Vlad Krasnokutskii <vladaccess@outlook.com>
2018-05-26 02:55:59 +0300

    create the outlets and link them

--- PartsProtein/Controllers/SettingsViewControllerTableViewController.swift
+++ PartsProtein/Controllers/SettingsViewControllerTableViewController.swift
@@ -11,7 +11,16 @@ import UIKit
 class SettingsViewControllerTableViewController: UITableViewController {
     
     let userDefaults = UserDefaults.groupUserDefaults()
-
+    
+    @IBOutlet weak var notificationSwitch:UISwitch!
+    @IBOutlet weak var healthSwitch:UISwitch!
+    @IBOutlet weak var smallPortionTF:UITextField!
+    @IBOutlet weak var bigPortionTF:UITextField!
+    @IBOutlet weak var goalTF:UITextField!
+    @IBOutlet weak var fromLabel:UILabel!
+    @IBOutlet weak var toLabel:UILabel!
+    @IBOutlet weak var everyLabel:UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Preferences"
