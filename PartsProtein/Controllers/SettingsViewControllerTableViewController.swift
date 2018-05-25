deaffff2d9db0d140efd23a3d0644395d4e02e4f
Vlad Krasnokutskii <vladaccess@outlook.com>
2018-05-26 01:56:59 +0300

    add the settingsVC to IB

---
+++ PartsProtein/Controllers/SettingsViewControllerTableViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SettingsViewControllerTableViewController.swift
+//  PartsProtein
+//
+//  Created by attom on 26.05.2018.
+//  Copyright © 2018 attom. All rights reserved.
+//
+
+import UIKit
+
+class SettingsViewControllerTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+
+
+}
