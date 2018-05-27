445066fa7c6028c08091d6e8d403de36e685c967
Vlad Krasnokutskii <vladaccess@outlook.com>
2018-05-27 18:00:57 +0300

    release the SmileScreen

--- PartsProtein/Controllers/FeedbackViewController.swift
+++ PartsProtein/Controllers/FeedbackViewController.swift
@@ -13,23 +13,20 @@ class FeedbackViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override var prefersStatusBarHidden: Bool {
+        return true
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func negativeAction() {
+        self.dismiss(animated: true)
     }
-    */
+    
+    @IBAction func sureAction() {
+        self.dismiss(animated: true) {
+//            UIApplication.shared.open(<#T##url: URL##URL#>, options: <#T##[String : Any]#>, completionHandler: <#T##((Bool) -> Void)?##((Bool) -> Void)?##(Bool) -> Void#>)
+        }
+    }
 
 }
